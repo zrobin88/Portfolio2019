f0987d8eab2990cfb99a294acf5a81a8456d571d
zrobin88 <zacharyrobin@Zacharys-MacBook-Pro-2.local>
2019-08-06 19:40:19 -0400

    the moon was doubled

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './style.css'
-// import img01 from '../IMG/p4.jpg';
+
 
 
 class Contact extends Component {
--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './style.css'
-// import img01 from '../IMG/p4.jpg';
+
 
 class Jumbotron extends Component {
 
